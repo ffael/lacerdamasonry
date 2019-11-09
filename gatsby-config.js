e69f514eb2d36c7db5d116fd4d147d8f4e2aa0fa
Rafael <ffael@live.com>
2019-11-09 15:29:27 -0500

    Added Basic Manifest and Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,49 @@ module.exports = {
      options:{
        displayName: true
      }
-   }
+   },
+   {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-114693120-3",
+      // Defines where to place the tracking script - `true` in the head and `false` in the body
+      head: false,
+      // Setting this parameter is optional
+      anonymize: true,
+      // Setting this parameter is also optional
+      respectDNT: true,
+      // Avoids sending pageview hits from custom paths
+      exclude: ["/preview/**", "/do-not-track/me/too/"],
+      // Delays sending pageview hits on route update (in milliseconds)
+      pageTransitionDelay: 0,
+      // // Enables Google Optimize using your container Id
+      // optimizeId: "GTM-NLRSPV7",
+      // // Enables Google Optimize Experiment ID
+      // experimentId: "UA-114693120-2",
+      // // Set Variation ID. 0 for original 1,2,3....
+      // variationId: "UA-114693120-2",
+      // Any additional optional fields
+      sampleRate: 5,
+      siteSpeedSampleRate: 10,
+      cookieDomain: "lacerdamasonry.com",
+    }
+   },
+   {resolve: `gatsby-plugin-manifest`,
+   options: {
+     name: "Lacerda Masonry",
+     short_name: "Lacerda",
+     start_url: "/",
+     background_color: "#fff",
+     theme_color: "#132554",
+     // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+     // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+     display: "standalone",
+     icon: "src/assets/img/favicon.png", // This path is relative to the root of the site.
+     // An optional attribute which provides support for CORS check.
+     // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+     // Any invalid keyword or empty string defaults to `anonymous`
+     crossOrigin: `use-credentials`,
+   },
+  },
   ]
 }
